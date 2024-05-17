ef73a3d9f77a91b1938ff49f18b5b536fb7099c1
Steve Korshakov <steve@korshakov.com>
2024-05-16 18:51:36 -0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Glassium Documentation
+
+TODO
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Glassium</span>,
+  project: {
+    link: 'https://glassium.org',
+  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/ex3ndr/glassium-docs',
+  footer: {
+    text: 'Bulka LLC, San Francisco, CA',
+  },
+}
+
+export default config
